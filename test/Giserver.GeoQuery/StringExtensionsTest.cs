f8ae802ad8cfe18c502b26ef066883767f74065f
cocaine_coder <tracywang1997@outlook.com>
2023-08-28 16:40:28 +0800

    增加Giserver.GeoQuery测试

---
+++ test/Giserver.GeoQuery/StringExtensionsTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giserver.GeoQuery;
+
+public class StringExtensionsTest
+{
+    [Fact]
+    public void ThrowIfNullTest()
+    {
+        string? value = null;
+
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            value.ThrowIfNullOrWhiteSpace(nameof(value));
+        });
+    }
+
+    [Fact]
+    public void ThrowIfWhiteSpaceTest()
+    {
+        string? value = "  ";
+
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            value?.ThrowIfNullOrWhiteSpace(nameof(value));
+        });
+    }
+
+    [Theory]
+    [InlineData(null, null)]
+    [InlineData("", null)]
+    [InlineData(" ", null)]
+    [InlineData("id,name", new[] { "id", "name" })]
+    public void SpliteByCommaTest(string? value, string[]? expected)
+    {
+        var result = value.SpliteByComma();
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ test/Giserver.GeoQuery/Usings.cs
@@ -0,0 +1,2 @@
+global using Xunit;
+global using Giserver.GeoQuery.Extensions;
