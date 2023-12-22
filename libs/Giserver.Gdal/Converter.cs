5946b1b614626075921db3a9146208912b574f6e
cocaine_coder <tracywang1997@outlook.com>
2023-12-22 17:42:55 +0800

    添加gdal代码

---
+++ libs/Giserver.Gdal/Converter.cs
@@ -0,0 +1,25 @@
+﻿using OSGeo.OGR;
+
+namespace Giserver.Gdal;
+
+public class Converter
+{
+    public static IEnumerable<Geometry>? VectorFileToGeometries(string path)
+    {
+        using var ds = Ogr.Open(path, 0);
+        if (ds != null)
+            for (var i = 0; i < ds.GetLayerCount(); i++)
+            {
+                var layer = ds.GetLayerByIndex(i);
+                if (layer == null) continue;
+
+                Feature? feature = layer.GetNextFeature();
+                while (feature != null)
+                {
+                    var geometry = feature.GetGeometryRef();
+                    yield return geometry;
+                    feature = layer.GetNextFeature();
+                }
+            }
+    }
+}
---
+++ test/UnitTest.Giserver.Gdal/ConverterTest.cs
@@ -0,0 +1,18 @@
+﻿using Giserver.Gdal;
+using MaxRev.Gdal.Core;
+
+namespace UnitTest.Giserver.Gdal;
+
+public class ConverterTest
+{
+    [Fact]
+    public void DxfConvertTest()
+    {
+        GdalBase.ConfigureAll();
+
+        string dxfFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "1.dxf");
+        string kmlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "1.kml");
+
+        var jsons = Converter.VectorFileToGeometries(kmlFile)!.Select(x => x.ExportToJson(null)).ToList();
+    }
+}
